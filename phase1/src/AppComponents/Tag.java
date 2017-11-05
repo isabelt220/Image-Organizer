b238f70ec14adbe5745a49ddc176ad98db0d484b
Micari <qikadate@aliyun.com>
2017-11-05 17:27:06 -0500

    Added new variables with appropriate getters and setters.

--- phase1/src/AppComponents/Tag.java
+++ phase1/src/AppComponents/Tag.java
@@ -1,4 +1,26 @@
 package AppComponents;
 
+import java.util.ArrayList;
+
 public class Tag {
+
+    private String name;
+    private ArrayList<ImageInfo> associatedImages;
+
+    public void setTagName(String name) {
+        this.name = name;
+    }
+
+    public void setAssociatedImages(ArrayList<ImageInfo> associatedImages) {
+        this.associatedImages = associatedImages;
+    }
+
+    public String getTagName() {
+
+        return name;
+    }
+
+    public ArrayList<ImageInfo> getAssociatedImages() {
+        return associatedImages;
+    }
 }
