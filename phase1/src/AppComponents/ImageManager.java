756862947b611245935ebc2dbf8db7d60cb7bd91
PiyushDatta <piyushdattaca@gmail.com>
2017-11-05 18:00:59 -0500

    Added ArrayList imageList to contain all the images created. Also made getters to update imageList. Made an addImage method to add images into the imageList ArrayList. - Piyush

--- phase1/src/AppComponents/ImageManager.java
+++ phase1/src/AppComponents/ImageManager.java
@@ -1,4 +1,24 @@
 package AppComponents;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/*
+Keeps track of all images and the info for each image
+*/
 public class ImageManager {
+
+    private ArrayList<ImageInfo> imageList;
+
+    public ImageManager() {
+        imageList = new ArrayList<>();
+    }
+
+    public void addImage(ImageInfo newImage) {
+        imageList.add(newImage);
+    }
+
+    public ArrayList<ImageInfo> getImageList() {
+        return imageList;
+    }
 }
