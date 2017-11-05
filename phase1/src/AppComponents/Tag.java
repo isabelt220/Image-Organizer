c369f773b45a1675dd89eb3d86a9e7d68294c280
Micari <qikadate@aliyun.com>
2017-11-05 17:51:15 -0500

    Add AddImage, which adds an image to the associateImages ArrayList, and RemoveImage, which deletes an image to the associateImages ArrayList. Also overrided the equals method for this class, so that we can check whether two images are the same based on their name.

--- phase1/src/AppComponents/Tag.java
+++ phase1/src/AppComponents/Tag.java
@@ -7,6 +7,19 @@ public class Tag {
     private String name;
     private ArrayList<ImageInfo> associatedImages;
 
+    protected void AddImage(ImageInfo image) {
+        this.associatedImages.add(image);
+    }
+
+    protected void RemoveImage(ImageInfo image) {
+        this.associatedImages.remove(image);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof Tag) && ((Tag) obj).name.equals(this.name);
+    }
+
     public void setTagName(String name) {
         this.name = name;
     }
