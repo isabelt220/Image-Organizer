7bc8c69ff21e005d02a089fafe371297f7f39464
PiyushDatta <piyushdattaca@gmail.com>
2017-11-16 15:54:10 -0500

    Added a new method called addTagExistingImage() that checks ImageManager's imageList to see if the image that is currently being viewed is in the imageList, if it is, we add the tag to that associated image by calling ImageManager. Also made a variable for the textfield beside the add tag button on the gui. Currently, this method only works if the object is exists in imageList, otherwise it calls the addTagNewImage method, which creates a new image and adds the tag to it. However this method is incomplete at the moment.

--- phase1/src/AppGUI/TreeView/ImageTagEditorController.java
+++ phase1/src/AppGUI/TreeView/ImageTagEditorController.java
@@ -1,13 +1,44 @@
 package AppGUI.TreeView;
 
+import AppComponents.ImageData;
+import AppComponents.ImageManager;
+import AppComponents.TagManager;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class ImageTagEditorController {
     @FXML
-     private ImageView myImageView = new ImageView();
+    private ImageView myImageView = new ImageView();
+    @FXML
+    private TextField addTagFieldTagEditor = new TextField();
 
     public void initialize() {
         myImageView.setImage(TreeViewController.selectedImage);
     }
+
+
+    public void addTagExistingImage() {
+        ImageData currImage = new ImageData(
+                TreeViewController.selectedImage.getUrl());
+
+        ArrayList<String> tagEditorTagList = new ArrayList<>();
+        tagEditorTagList.add(0, addTagFieldTagEditor.getText());
+
+        for (ImageData i : ImageManager.getImageList()) {
+            if (((ImageData) i).equals(currImage)) {
+                ImageManager.imAddTagWithImage(i, tagEditorTagList);
+                return;
+            }
+        }
+
+        this.addTagNewImage();
+    }
+
+    public void addTagNewImage(){
+        // Need to complete.
+    }
+
 }
