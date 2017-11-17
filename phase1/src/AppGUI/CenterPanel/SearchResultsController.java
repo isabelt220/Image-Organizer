f13392e006cc7136a8c2368665a9aed7c0e17b1d
Siyi <siyic.wang@mail.utoronto.ca>
2017-11-17 12:06:17 -0500

    Added (currently commented out) methods to add a image preview column to searchresults window, deleted path column and changed name colunm to also show path

--- phase1/src/AppGUI/CenterPanel/SearchResultsController.java
+++ phase1/src/AppGUI/CenterPanel/SearchResultsController.java
@@ -1,14 +1,20 @@
 package AppGUI.CenterPanel;
 
 import AppComponents.ImageData;
+import javafx.beans.InvalidationListener;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.util.*;
 
 public class SearchResultsController {
 
@@ -18,26 +24,55 @@ public class SearchResultsController {
     private static ObservableList<ImageData> images;
     @FXML
     public TableView<ImageData> resultTable;
-    @FXML
-    public TableColumn<ImageData,String> imagePath;
+//    @FXML
+//    public TableColumn<ImageData,String> imagePath;
     @FXML
     public TableColumn<ImageData, String> imageName;
+    @FXML
+    public TableColumn<ImageData, ImageView> imagePreview;
 
 
     @FXML
     public void initialize() {
-        imagePath.setCellValueFactory(cellData -> cellData.getValue().getPathProperty());
+        imagePreview.setCellValueFactory(new PropertyValueFactory<>("picture"));
+        imagePreview.setCellFactory(column -> {
+            return new TableCell<ImageData, ImageView>() {
+                @Override
+                protected void updateItem(ImageView image, boolean empty) {
+                    super.updateItem(image, empty);
+                    if (image == null || empty) {
+                        setText(null);
+                        setStyle("");
+                    } else {setGraphic(image);
+                    }
+                }
+            };
+        });
+//        imagePath.setCellValueFactory(cellData -> cellData.getValue().getPathProperty());
         imageName.setCellValueFactory(cellData -> cellData.getValue().getNameProperty());
         SortedList<ImageData> sortedData = new SortedList<>(images);
         sortedData.comparatorProperty().bind(resultTable.comparatorProperty());
         resultTable.setItems(sortedData);
-        
+//        ArrayList<TableListElement> imageTable =  new ArrayList<>();
+//        for(ImageData pic: images){
+//            Image image2 = new Image(pic.getImageLocation(),100,100,true,true);
+//                ImageView tableImage = new ImageView();
+//                tableImage.setImage(image2);
+//                TableListElement newElement = new TableListElement(pic.getImageLocation());
+//                newElement.setPicture(tableImage);
+//                imageTable.add(newElement);
+//            }
+//        }
+////        resultTable.get
+////        tableView.setItems(FXCollections.observableList(imageTable));
 
-    }
 
-    public static void setImages(ObservableList<ImageData> oImages){
-        images = oImages;
-    }
-
-
+//
 }
+
+    public static void setImages(ObservableList<ImageData> iImages){
+        images = iImages;
+    }}
+
+
+
