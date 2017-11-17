7e8aa0e45f7abd6b4b7c814594e0214dbcfd3640
Isabel Tang <isabeltang@Isabels-MacBook-Pro.local>
2017-11-17 12:26:25 -0500

    Deleted old code and added new methods in TopPanel.java for new menu items added in MainView.fxml.

--- phase1/src/AppGUI/TopPanel.java
+++ phase1/src/AppGUI/TopPanel.java
@@ -2,6 +2,7 @@ package AppGUI;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.geometry.Orientation;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
@@ -11,41 +12,24 @@ import javafx.scene.layout.Pane;
 
 
 
-public class TopPanel extends MenuBar {
+public class TopPanel {
+    @FXML
+    private MenuItem exitButton;
 
+    public void addNewTag() {
+        MainGUI.treeViewController.addTagClick();
+    }
 
-    private Menu editButton;
-    private Menu helpButton;
+    public void deleteExistingTag() {
+        MainGUI.treeViewController.deleteTagClick();
+    }
 
-    public TopPanel(){
+    public void openImageTagEditor() throws Exception {
+        MainGUI.treeViewController.openImageTagEditor();
+    }
 
-        Menu fileButton = new Menu("File");
-        editButton = new Menu("Edit");
-        helpButton = new Menu("Help");
-        MenuItem fileOpen = new CustomMenuItem(new Label("Open"));
-        CustomMenuItem fileClose = new CustomMenuItem(new Label("Close"));
-        CustomMenuItem editTags = new CustomMenuItem(new Label("Tags"));
-        CustomMenuItem editLocation = new CustomMenuItem(new Label("Location"));
-        CustomMenuItem helpTerminate = new CustomMenuItem(new Label("Terminate"));
-        CustomMenuItem helpGuide = new CustomMenuItem(new Label("UserGuide"));
-        fileButton.getItems().addAll(fileOpen, fileClose);
-        editButton.getItems().addAll(
-                editTags,
-                editLocation
-        );
-        helpButton.getItems().addAll(
-                helpGuide,
-                helpTerminate
-        );
-
-//        fileOpen.setOnAction(event -> );
-//        fileClose.setOnAction(event -> );
-//        editTags.setOnAction(event -> );
-//        editLocation.setOnAction(event -> );
-//        helpGuide.setOnAction(event -> );
-//        helpTerminate.setOnAction(event -> );
-        this.getMenus().addAll(fileButton, editButton, helpButton);
-
+    public void openChooseDirectory() {
+        MainGUI.treeViewController.openFolder();
     }
 
 }
