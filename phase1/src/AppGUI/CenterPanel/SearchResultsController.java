6836748d08f98490f65f2a546cb1525422f776d8
Siyi <siyic.wang@mail.utoronto.ca>
2017-11-18 13:46:07 -0500

    Implemented image view in Search results, the user can now preview the images when they search for a tag

--- phase1/src/AppGUI/CenterPanel/SearchResultsController.java
+++ phase1/src/AppGUI/CenterPanel/SearchResultsController.java
@@ -2,6 +2,8 @@ package AppGUI.CenterPanel;
 
 import AppComponents.ImageData;
 import javafx.beans.InvalidationListener;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -12,6 +14,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.util.Callback;
 
 import java.io.File;
 import java.util.*;
@@ -23,25 +26,26 @@ public class SearchResultsController {
 //    public TreeTableColumn imagePreview;
     private static ObservableList<ImageData> images;
     @FXML
-    public TableView<ImageData> resultTable;
+    public TableView<TableListElement> resultTable;
 //    @FXML
 //    public TableColumn<ImageData,String> imagePath;
     @FXML
-    public TableColumn<ImageData, String> imageName;
+    public TableColumn<TableListElement, String> imageName;
     @FXML
-    public TableColumn<ImageData, ImageView> imagePreview;
+    public TableColumn<TableListElement, ImageView> imagePreview;
 
 
     @FXML
     public void initialize() {
         imagePreview.setCellValueFactory(new PropertyValueFactory<>("picture"));
         imagePreview.setCellFactory(column -> {
-            return new TableCell<ImageData, ImageView>() {
+            return new TableCell<TableListElement, ImageView>() {
                 @Override
                 protected void updateItem(ImageView image, boolean empty) {
                     super.updateItem(image, empty);
                     if (image == null || empty) {
                         setText(null);
+                        setGraphic(null);
                         setStyle("");
                     } else {setGraphic(image);
                     }
@@ -49,10 +53,16 @@ public class SearchResultsController {
             };
         });
 //        imagePath.setCellValueFactory(cellData -> cellData.getValue().getPathProperty());
-        imageName.setCellValueFactory(cellData -> cellData.getValue().getNameProperty());
-        SortedList<ImageData> sortedData = new SortedList<>(images);
-        sortedData.comparatorProperty().bind(resultTable.comparatorProperty());
-        resultTable.setItems(sortedData);
+//        imageName.setCellValueFactory(cellData -> cellData.getValue().getName());
+        imageName.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<TableListElement, String>,
+                ObservableValue<String>>() {
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<TableListElement, String> t) {
+                return new SimpleStringProperty(t.getValue().getName());
+            }
+        });setResults();
+//        SortedList<ImageData> sortedData = new SortedList<>(images);
+//        sortedData.comparatorProperty().bind(resultTable.comparatorProperty());
+//        resultTable.setItems(sortedData);
 //        ArrayList<TableListElement> imageTable =  new ArrayList<>();
 //        for(ImageData pic: images){
 //            Image image2 = new Image(pic.getImageLocation(),100,100,true,true);
@@ -72,7 +82,26 @@ public class SearchResultsController {
 
     public static void setImages(ObservableList<ImageData> iImages){
         images = iImages;
-    }}
+    }
+
+    public void setResults(){
+        ArrayList<TableListElement> imageTable =  new ArrayList<>();
+        for(ImageData pic: images){
+            File f = new File(pic.getLocation());
+            String url = f.toPath().toString();
+            Image image2 = new Image(f.toURI().toString(),100,100,true,true);
+            ImageView tableImage = new ImageView();
+            tableImage.setImage(image2);
+
+            String temp = f.getName();
+            TableListElement newElement = new TableListElement(temp.substring(0,temp.lastIndexOf(".")));
+            newElement.setPicture(tableImage);
+            imageTable.add(newElement);
+        }
+        resultTable.getColumns().addAll(imagePreview,imageName);
+        resultTable.setItems(FXCollections.observableList(imageTable));
+    }
+}
 
 
 
