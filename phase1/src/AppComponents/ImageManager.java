38b8a0e7d88419f0a22fe6ecc7de58146c63b0a2
Micari <qikadate@aliyun.com>
2017-11-18 01:31:19 -0500

    Added new methods in ImageManager to seriablize the imagelist

--- phase1/src/AppComponents/ImageManager.java
+++ phase1/src/AppComponents/ImageManager.java
@@ -1,5 +1,9 @@
 package AppComponents;
 
+import java.awt.*;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /*
@@ -10,7 +14,68 @@ public class ImageManager {
     private static ArrayList<ImageData> imageList = new ArrayList<>();
 
     public ImageManager() {
+        Path currentRelativePath = Paths.get("");
+        String filePath = currentRelativePath.toAbsolutePath().toString();
+        filePath += "/imageConfig.txt";
+
+        File file = new File(filePath);
+        if (file.exists() && file.length() != 0) {
+            readImagesFromFile(filePath);
+        } else {
+            try{
+                file.createNewFile();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
+
+    public void readImagesFromFile(String filePath) {
+        try {
+            FileInputStream is = new FileInputStream(filePath);
+            ObjectInputStream os = new ObjectInputStream(is);
+
+            int num = os.readInt();
+
+            for (int i=0; i < num; i++) {
+                ImageData image = (ImageData) os.readObject();
+                imageList.add(image);
+            }
+
+            os.close();
+            is.close();
+
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveTagsToFile(String filePath) {
+        try {
+            FileOutputStream fs = new FileOutputStream(filePath);
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+
+            os.writeInt(imageList.size());
+
+            for (ImageData image : imageList) {
+                os.writeObject(image);
+            }
+
+            os.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 
     public void addImage(ImageData newImage) {
         imageList.add(newImage);
@@ -20,16 +85,16 @@ public class ImageManager {
         return imageList;
     }
 
-    public void imAddTagWithImage(ImageData currImage, ArrayList<String> tagNameList) {
+    public ImageData imAddTagWithImage(ImageData currImage, ArrayList<String> tagNameList) {
         for (ImageData i : imageList) {
             if (i.equals(currImage)) {
                 ArrayList<Tag> newTags = TagManager.tmAddTagWithImage(tagNameList);
                 i.setImageTags(newTags);
-                return;
+                return i;
             }
         }
 
-        imAddTagNewImage(currImage, tagNameList);
+        return imAddTagNewImage(currImage, tagNameList);
     }
 
     public boolean ImageExist(String location){
@@ -47,9 +112,10 @@ public class ImageManager {
         return null;
     }
 
-    private void imAddTagNewImage(ImageData currImage, ArrayList<String> tagNameList) {
+    private ImageData imAddTagNewImage(ImageData currImage, ArrayList<String> tagNameList) {
         imageList.add(currImage);
         ArrayList<Tag> newTags = TagManager.tmAddTagWithImage(tagNameList);
         currImage.addTags(newTags);
+        return currImage;
     }
 }
