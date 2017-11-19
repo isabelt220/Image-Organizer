6ba6c07d21c3412d8d3f9fd825ed83748395a437
PiyushDatta <piyushdattaca@gmail.com>
2017-11-19 18:08:05 -0500

    Took out unused imports. Also made the window show in TreeViewItem incase the directory is null or there is an exception. Small changes.

--- phase1/src/AppGUI/CenterPanel/SearchResults.java
+++ phase1/src/AppGUI/CenterPanel/SearchResults.java
@@ -2,35 +2,29 @@ package AppGUI.CenterPanel;
 
 import AppComponents.ImageData;
 import AppGUI.MainContainer;
-import AppGUI.MainGUI;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import sun.applet.Main;
 
 import java.util.ArrayList;
 
 public class SearchResults {
     private Stage mainStage;
-    private AnchorPane mainLayout;
 
-    public void display(ArrayList<ImageData> imageList) throws Exception{
-        if(MainContainer.getSearchResultsController()==null){
-        mainStage = new Stage();
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(SearchResults.class.getResource("SearchResults.fxml"));
-        mainLayout = loader.load();
-        mainStage.setScene(new Scene(mainLayout));
-        MainContainer.setSearchResultsController((loader.getController()));
-        MainContainer.getSearchResultsController().setResults(imageList);
-        mainStage.show();
-    }else{
+    public void display(ArrayList<ImageData> imageList) throws Exception {
+        if (MainContainer.getSearchResultsController() == null) {
+            mainStage = new Stage();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(SearchResults.class.getResource("SearchResults.fxml"));
+            AnchorPane mainLayout = loader.load();
+            mainStage.setScene(new Scene(mainLayout));
+            MainContainer.setSearchResultsController((loader.getController()));
             MainContainer.getSearchResultsController().setResults(imageList);
-          mainStage.show();
+            mainStage.show();
+        } else {
+            MainContainer.getSearchResultsController().setResults(imageList);
+            mainStage.show();
         }
     }
 
