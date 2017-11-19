44f644cd1fc98f187d9b4958f3d8afbc5415251b
Siyi <siyic.wang@mail.utoronto.ca>
2017-11-19 08:52:07 -0500

    Cleaned unnessessary code from ImageData, and added docstrings to each method, for clarity and peace of mind

--- phase1/src/AppComponents/ImageData.java
+++ phase1/src/AppComponents/ImageData.java
@@ -2,9 +2,6 @@ package AppComponents;
 
 
 import AppGUI.MainContainer;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
 import java.io.File;
 import java.io.Serializable;
 import java.sql.Timestamp;
@@ -14,20 +11,28 @@ import java.util.Map;
 
 public class ImageData implements Serializable{
 
-
+    // String Location is the complete path of the file. eg. C://User/phase1/Christmas.png
     private String location = "";
-    private String name = "";
-    private String coreName = "";
-    private ArrayList<Tag> tagList;
-//  String: timestamp, String: name of image, or location
-    private LinkedHashMap<String, String> nameLog = new LinkedHashMap<>();
-//    private static int idCounter;
-//    private int id;
-    //For testing purposes
-    private String lastChangeTime;
+    // String path is a substring of location and is the path of the image file that this ImageData is attached to. eg. C://User/phase1/
     private String path;
+    // String path is a substring of location and is the type of the image file that this ImageData is attached to. eg. .png, .img, .jpg
     private String type;
+    //String name is the substring of location after path and before extension, it can be changed with the editing of tags. eg. Christmas @2017 @Mistletoe
+    private String name = "";
+    //String coreName is the substring of location after path and before extension,it is set in the constructor and does not change. eg. Christmas
+    private String coreName = "";
+    //tagList is the ArrayList of tags that are associated with the image
+    private ArrayList<Tag> tagList;
+    //nameLog is a ordered hashmap with a timestamp of each tag modification paired to the image name at that timestamp.
+    private LinkedHashMap<String, String> nameLog = new LinkedHashMap<>();
+    //for testing purposes only
+    private String lastChangeTime;
 
+    /**
+     * Sets up an ImageData Object, initializes the value name, coreName, type, tagList, and adds the initial entry to
+     * nameLog.
+     * @param location String
+     */
     public ImageData(String location) {
         this.location = location;
         File imageFile = new File(location);
@@ -38,20 +43,34 @@ public class ImageData implements Serializable{
         path = location.substring(0,x);
         int i = imageFile.getName().lastIndexOf('.');
         type = imageFile.getName().substring(i+1);
-//        setImageLocation(location);
         Timestamp time = new Timestamp(System.currentTimeMillis());
         nameLog.put(time.toString(), "Initially named : "+ name);
         tagList = new ArrayList<>();
-//        setImageID(idCounter);
-//        idCounter++;
     }
 
+    /**
+     * Getter for nameLog, returns it in the form of a LinkedHashMap, is generally called upon by classes that implements
+     * the display of a history of changes made to an ImageData.
+     * @return LinkedHashMap<String,String>
+     */
     public LinkedHashMap<String, String> getNameLog(){return nameLog;}
 
+    /**
+     * Getter for name, returns it in the form of String, is used to obtain the current name of the ImageData.
+     * @return String
+     */
     public String getName(){return name;}
 
+    /**
+     * Getter for coreName, returns it in the form of String, is used to obtain the non-changing coreName of the ImageData.
+     * Can be used generate new names with tag addition and deletion.
+     * @return String
+     */
     public String getCoreName(){return coreName;}
 
+    /**
+     *Solely for Testing Purposes
+     */
     public String printLog(){
         String log = "";
         for (Map.Entry<String, String> entry : nameLog.entrySet()){
@@ -60,72 +79,65 @@ public class ImageData implements Serializable{
             log += key + "---" + value;
             log += System.getProperty("line.separator");
         }
-        //for (String entry: nameLog.keySet()){
-        //    log += entry + "---" + nameLog.get(entry);
-        //    log += System.getProperty("line.separator");
-        //}
         return log;
 
     }
 
-//    private void setImageID(int IdCounter){
-//        id = IdCounter;
-//
-//    }
 
-//    public int getImageID(){
-//        return id;
-//    }
-
+    /**
+     * Iterates through the tags in dTags, checks for existence in tagList, and deletes it if it exists, removing this
+     * ImageData from association of deleted tags.
+     * Calls setImageTags to reset the name of this ImageData according to current Tags.
+     * @param dTags ArrayList<Tag></>
+     */
    public void deleteTags(ArrayList<Tag> dTags){
-//        for (int i = 0; i < dTags.size(); i++) {
-//            if((tagList.contains(dTags.get(i)))){
-//                tagList.remove(dTags.get(i));}
-//            }
         for (Tag dTag : dTags) {
             if ((tagList.contains(dTag))) {
                 tagList.remove(dTag);
+                dTag.removeImage(this);
             }
         }
-        //goes to Isabel and Pyush
         setImageTags(tagList);
 
     }
 
+    /**
+     * Deletes all tags from tagList, and removes association of this image from all Tags it was associated by
+     * NOT USED BUT PLEASE DO NOT DELETE
+     * May be used for additional functions in phase2
+     */
     public void deleteAllTags(){
+        for(Tag tag : tagList){
+            tag.removeImage(this);
+        }
         tagList.clear();
         setImageTags(tagList);
     }
 
+    /**
+     * Iterates through the tags in newTags, checks for existence in tagList, and adds it if it doesn't exists, adding this
+     * ImageData to association of new tags.
+     * Calls setImageTags to reset the name of this ImageData according to current Tags.
+     * @param newTags ArrayList<Tag>
+     */
     void addTags(ArrayList<Tag> newTags){
         for (Tag newTag : newTags) {
             if (!(tagList.contains(newTag))) {
                 tagList.add(newTag);
+                newTag.addImage(this);
             }
         }
-//        for (int i = 0; i < newTags.size(); i++) {
-//            if(!(tagList.contains(newTags.get(i)))){
-//                tagList.add(newTags.get(i));
-//            }
-//        }
         setImageTags(tagList);
     }
 
 
-//    public void setImageLocation(String imageLocation){
-//        Timestamp time = new Timestamp(System.currentTimeMillis());
-//        if (location.length() == 0){
-//            location = imageLocation;
-//            lastChangeTime = time.toString();
-//        }
-//        else{nameLog.put(time.toString(), "location change: " + location + " --> " + imageLocation);
-//            File before = new File(location);
-//            boolean after = before.renameTo(new File(imageLocation + before.getName()));
-//            location = imageLocation + name;
-//            lastChangeTime = time.toString();
-//            location = imageLocation; }
-//    }
-
+    /**
+     * Takes the coreName of this ImageData, and iterates through tags to concatenate a string according to specified format.
+     * Adds tags to tagList if they are not in there already
+     * Calls tmAddTagWithImage in TagManager to add tag and association of this ImageData with said tag.
+     * Calls setImageName to change the name to concatenated string.
+     * @param tags ArrayList<Tag>
+     */
     public void setImageTags(ArrayList<Tag> tags) {
         StringBuilder compressedTags = new StringBuilder(coreName);
         ArrayList<String> stringVer= new ArrayList<>();
@@ -136,17 +148,19 @@ public class ImageData implements Serializable{
             }
             compressedTags.append(" @" + tag.getTagName());
         }
-        ArrayList<Tag> temp = MainContainer.getAppTagManager().tmAddTagWithImage(this, stringVer);
-//        for (int i = 0; i < tags.size(); i++) {
-//            compressedTags.append(" @" + tags.get(i).getTagName());
-//        }
+        MainContainer.getAppTagManager().tmAddTagWithImage(this, stringVer);
         setImageName(compressedTags.toString());
-//            nameLog.put(time.toString(), "tag change: " + name + " --> " + compressedTags.toString());
-//            name = compressedTags.toString();
-//            lastChangeTime = time.toString();}
 
     }
 
+    /**
+     * Adds an entry in the nameLog with the current timestamp mapped to a string in the format of
+     * tag change: oldName --> newName
+     * Creates two files each with the location of oldName and newName, and uses the renameTo() method in
+     * java File to rename the image file that this ImageData is attached to.
+     * Modifies the name and location of the ImageData in accordance with the image file this ImageData is attached to.
+     * @param newName String
+     */
     private void setImageName(String newName) {
         Timestamp time = new Timestamp(System.currentTimeMillis());
          nameLog.put(time.toString(), "tag change: " + name + " --> " + newName);
@@ -156,39 +170,58 @@ public class ImageData implements Serializable{
             name = newName;
             location = path+name+"."+type;
             lastChangeTime = time.toString();
-//            if(flag){
-//                //image changed successfully
-//            }else{}
-//                //image rename fails
 }
 
-
+    /**
+     * Getter for location, returns it in the form of String, can be used to create and manipulate image files
+     * that this ImageData is attached to.
+     * @return String
+     */
     public String getLocation() {
         return location;
     }
 
 
+    /**
+     * Getter for tagList, returns it in the form of ArrayList<Tag>, can be used to iterate through current tags of image.
+     * NOT USED BUT PLEASE DON'T DELETE
+     * @return ArrayList<Tag>
+     */
     public ArrayList<Tag> getImageTags(){
         return tagList;
     }
 
-    public boolean hasTag(String Tagname){
-        Tag targetTag = new Tag(Tagname);
-        return tagList.contains(targetTag);
+    /**
+     * Checks if this ImageData is associated with a tag of name tagName,
+     * double checks if the tagList contains the target tag and if this ImageData is in the associated image list of this
+     * ImageData.
+     * @param tagName String
+     * @return boolean
+     */
+    public boolean hasTag(String tagName){
+        Tag targetTag = new Tag(tagName);
+        return tagList.contains(targetTag)&& targetTag.getAssociatedImages().contains(this);
     }
 
-    private String getImageLocation() {
-        return location;
-    }
 
-    //For testing purposes
+    /**
+     * Getter for the lastChangeTime of the ImageData name.
+     * FOR TESTING PURPOSES PLEASE DO NOT DELETE
+     * @return String
+     */
     public String getLastChangeTime() {
         return lastChangeTime;
     }
 
 
+    /**
+     * Overrides equals method in Object, check if other Object is an ImageData and if it has the same location
+     * as this ImageData object (location of ImageData objects should always be unique).
+     * @param other Object
+     * @return boolean
+     */
     @Override
     public boolean equals(Object other){
-        return other instanceof ImageData && location.equals(((ImageData) other).getImageLocation());
+        return other instanceof ImageData && location.equals(((ImageData) other).getLocation());
     }
 }
