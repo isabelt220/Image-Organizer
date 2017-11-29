b4f093315e122072d7e749e0c5910c8eba402f88
Siyi <siyic.wang@mail.utoronto.ca>
2017-11-29 11:39:38 -0500

    Initialized template for the master log of all tag modifications of image manager

--- phase2/src/AppGUI/CenterPanel/SearchResultsController.java
+++ phase2/src/AppGUI/CenterPanel/SearchResultsController.java
@@ -15,7 +15,7 @@ public class SearchResultsController extends FolderPanelController {
      * Set the results of the table when they press the search button.
      * Set the results to the observableList, which has the associatedImagesList
      * for each tag
-     * 
+     *
      * @param result ArrayList<ImageData> that fits the criteria of the search.
      */
     public void setResults(ArrayList<ImageData> result) {
