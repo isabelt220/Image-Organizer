9ddeefbcb53bd637a1854fa949a2d1b22a85b1e6
Siyi <siyic.wang@mail.utoronto.ca>
2017-11-29 19:34:13 -0500

    Added docstring and reformatted all classes in Observers

--- phase2/src/Observers/SearchResultObserver.java
+++ phase2/src/Observers/SearchResultObserver.java
@@ -6,17 +6,34 @@ import AppGUI.CenterPanel.SearchResultsController;
 import javax.naming.directory.SearchResult;
 import java.util.ArrayList;
 
+/** SubClass of Observer, observes an SearchResults Controller
+ */
 public class SearchResultObserver extends Observer {
 
+    /** The SearchResultsController that this SearchResults observes*/
     private SearchResultsController target;
 
-    public void setTarget(SearchResultsController target) {
-        this.target = target;
-    }
 
+    /**
+     * Setter for the results of the user input search for the target SearchResultsController that this SearchResultsObserver observes.
+     *
+     * @param imageList results of the user tag search.
+     */
     public void displayResults(ArrayList<ImageData> imageList){
         if (target != null){
             target.setResults(imageList);
         }
     }
+
+    /**
+     * Setter for the target SearchResultsController that this SearchResultsObserver observes.
+     *
+     * @param target SearchResultsController to observe.
+     */
+    public void setTarget(SearchResultsController target) {
+
+        this.target = target;
+    }
+
+
 }
